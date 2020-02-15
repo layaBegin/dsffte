670d3ce80aa307dc720d03daceadae69d04a58a0
yunteng <maweiphilippines@gmail.com>
2020-02-15 11:15:59 +0800

    sdfds
    sdaf

---
+++ assets/Script/CommitData.ts
@@ -0,0 +1,39 @@
+import NetworkLogic from "../../Shared/NetworkLogic";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class CommitData extends cc.Component {
+
+    @property(cc.Node)
+    content: [cc.Node] = [];
+    @property(cc.Button)
+    btnSubmit: cc.Button = null;
+
+    data = null;
+
+    onLoad () {
+        this.node.on("COMMITDATA",this.updateUI.bind(this))
+    }
+
+    onEnable () {
+        console.log("=====子界面打开")
+
+    }
+    updateUI(data){
+        this.data = data;
+        console.log("=====提交方式 data:",data);
+        for (let i  = 0; i < this.content.length;i++){
+            this.content[i].active = false;
+        }
+        this.content[data - 1].active = true;
+    }
+
+    onBtnClick(event,param){
+        this.node.active = false;
+    }
+
+    onDestroy(): void {
+        this.node.off("COMMITDATA")
+    }
+}
